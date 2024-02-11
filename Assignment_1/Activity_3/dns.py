22a8dd0ee02e0ca3048e287b81a169fa896f2ff5
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2024-02-11 13:31:19 +0530

    done: Query question

--- Assignment_1/Activity_3/dns.py
+++ Assignment_1/Activity_3/dns.py
@@ -1,3 +1,15 @@
+class DNSPacket:
+    def __init__(self, bitStream) -> None:
+        if len(bitStream): bitStream = "0" + bitStream # leftPading the bitStream to get octects
+        self.bitStream = bitStream
+        if len(self.bitStream) % 8: raise Exception("Invalid data")
+        self.header = Header(bitStream[:96])
+        bitStream = bitStream[96:]
+        self.question = Question(bitStream)
+        print(self.question.domain)
+        bitStream = bitStream[self.question.totalLength:]
+        self.answer = ResourceRecord(bitStream)
+
 class Header:
     def __init__(self, headerString) -> None:
         header = [headerString[i : i + 16] for i in range(0, len(headerString), 16)]
@@ -8,11 +20,8 @@ class Header:
         self.nsCount = header[4]
         self.arCount = header[5]
 
-class Question:
-    def __init__(self, bitStream) -> None:
-        self.domain = self.extractDomain(bitStream)
-    
-    def extractDomain(self, bitStream) -> str:
+class Payload:
+    def extract(self, bitStream):
         i = 0
         labels = []
         while bitStream[i : i + 8] != "0" * 8:
@@ -24,16 +33,25 @@ class Question:
                 j += 8
             labels.append(label)
             i += (lengthOctet + 1) * 8
-        return ".".join(labels)
+        i += 8
+        return ".".join(labels), i
+
+class Question(Payload):
+    def __init__(self, bitStream) -> None:
+        self.domain, self.nameLength = super().extract(bitStream)
+        self.totalLength = self.nameLength + 2 * 16 # QTYPE and QCLASS
+    
+class ResourceRecord(Payload):
+    def __init__(self, bitStream) -> None:
+        self.resource, self.resourceLength = super().extract(bitStream)
+        self.totalLength = self.resourceLength + 7 * 16 # Assuming A record an class IN
 
 if __name__ == "__main__":
     try:
         bitStream = bin(int(input("Enter DNS hexadecimcal byte stream: "), 16))[2:]
+        dnsPacket = DNSPacket(bitStream)
+        print(dnsPacket.question.domain)
+        if dnsPacket.answer.resource: print(dnsPacket.answer.resource)
     except:
-        print("Enter a valid hexadecimal byte stream")
+        print("Invalid data")
         exit()
-    if len(bitStream) % 2: bitStream = "0" + bitStream
-    header = Header(bitStream[:96])
-    bitStream = bitStream[96:]
-    question = Question(bitStream)
-    print(question.domain)
