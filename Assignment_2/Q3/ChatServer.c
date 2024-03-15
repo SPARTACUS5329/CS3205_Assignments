bd384f2ca1237dcb3854795f1b92a01d969529e1
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2024-03-15 11:32:18 +0530

    removed unnecessary brackets

--- Assignment_2/Q3/ChatServer.c
+++ Assignment_2/Q3/ChatServer.c
@@ -9,6 +9,7 @@
 
 char clientMessage[2000];
 char buffer[1024];
+static int userCount = 0;
 
 void error(const char *msg) {
 	perror(msg);
@@ -20,9 +21,7 @@ void* socketThread(void *args) {
 
  	do {
  		bzero(buffer, 255);
- 		if (read(newSocket, buffer, 255) < 0) {
- 			error("Error in reading");
- 		}
+ 		if (read(newSocket, buffer, 255) < 0) error("Error in reading");
  
  		printf("Client: %s\n", buffer);
 		send(newSocket, buffer, 13, 0);
@@ -35,9 +34,7 @@ void* socketThread(void *args) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		error("Not enough input arguments\n");
-	}
+	if (argc < 2) error("Not enough input arguments\n");
 	
 	int sockfd, newSockfd, portno;
 	char buffer[255];
@@ -47,9 +44,7 @@ int main(int argc, char *argv[]) {
 	socklen_t cliLen;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		error("Error in opening socket");
-	}
+	if (sockfd < 0) error("Error in opening socket");
 	bzero((char *) &servAddr, sizeof(servAddr));
 	portno = atoi(argv[1]);
 
@@ -57,27 +52,24 @@ int main(int argc, char *argv[]) {
 	servAddr.sin_addr.s_addr = INADDR_ANY;
 	servAddr.sin_port = htons(portno); // host to network short
 
-	if (bind(sockfd, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0) {
-		error("Binding failed");
-	}
+	if (bind(sockfd, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0) error("Binding failed");
 
 	if (listen(sockfd, 5) == 0) printf("Listening to PORT %d\n", portno);
 	else error("[Error in listening]");
 	
 	pthread_t clientIDs[5];
-	int i = 0;
 
 	while (1) {
 		cliLen = sizeof(cliAddr);
 		newSockfd = accept(sockfd, (struct sockaddr*) &cliAddr, &cliLen);
 		printf("Reaching\n");
-		if (pthread_create(&clientIDs[i++], NULL, socketThread, &newSockfd) != 0) error("[Thread creation]\n");
-		if (i >= 5) {
-			i = 0;
-			while (i < 5) {
-				pthread_join(clientIDs[i++], NULL);
+		if (pthread_create(&clientIDs[userCount++], NULL, socketThread, &newSockfd) != 0) error("[Thread creation]\n");
+		if (userCount >= 5) {
+			userCount = 0;
+			while (userCount < 5) {
+				pthread_join(clientIDs[userCount++], NULL);
 			}
-			i = 0;
+			userCount = 0;
 		}
 	}
 
