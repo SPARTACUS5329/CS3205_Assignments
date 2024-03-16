f06e2b02b7b810f0ccfee00acc7eaf343d4b96c0
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2024-03-16 19:30:49 +0530

    sending name tag with message

--- Assignment_2/Q3/ChatServer.c
+++ Assignment_2/Q3/ChatServer.c
@@ -21,27 +21,28 @@ void error(const char *msg) {
 void* userThread(void *args) {
 	int newSocket = *((int *) args);	
 	char buffer[MAX_MESSAGE_LENGTH];
+	char tempBuffer[MAX_MESSAGE_LENGTH];
 	user_t user = users[userCount - 1];
 	user.id = newSocket;
 	char usersString[MAX_USERS * MAX_MESSAGE_LENGTH];
-	// for (int i = 0; i < MAX_USERS; i++) {
-		// strcat(usersString, users[i].name);
-	// }
+	for (int i = 0; i < MAX_USERS; i++) {
+		strcat(usersString, users[i].name);
+	}
+	printf("userString: %s\n", usersString);
 	// send(user.sockfd, usersString, strlen(usersString), 0);
 	send(user.sockfd, "Connection successful", 21, 0);
  	if (read(newSocket, user.name, MAX_MESSAGE_LENGTH) < 0) error("Error in reading");
-	printf("%s\n", user.name);
 	for (int i = 0; i < MAX_USERS; i++) {
 		if (users[i].id == user.id) continue;
-		printf("%d %d %d %d\n", i, users[i].id, user.id, users[i].sockfd);
 		send(users[i].sockfd, user.name, MAX_MESSAGE_LENGTH, 0);
 	}
 	printf("Reaching here\n");
  	do {
  		bzero(buffer, MAX_MESSAGE_LENGTH);
- 		if (read(newSocket, buffer, MAX_MESSAGE_LENGTH) < 0) error("Error in reading");
- 
- 		printf("Client: %s\n", buffer);
+		strcpy(buffer, user.name); 
+		strcat(buffer, ": "); 
+ 		if (read(newSocket, tempBuffer, MAX_MESSAGE_LENGTH) < 0) error("Error in reading");
+		strcat(buffer, tempBuffer);
 		for (int i = 0; i < MAX_USERS; i++) {
 			if (users[i].id == user.id) continue;
 			send(users[i].sockfd, buffer, MAX_MESSAGE_LENGTH, 0);
